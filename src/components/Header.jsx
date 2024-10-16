a4352e235ccdabcace872b67b28d109a4076a879
Kim YS <withseven87@gmail.com>
2024-10-16 11:46:13 +0900

    인트로완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,66 +1,66 @@
 import React,{useState} from "react";
 const headerNav = [
   {
-    title: "intro",
-    url: "#intro",
+    title:"intro",
+    url:"#intro",
   },
   {
-    title: "skill",
-    url: "#skill",
+    title:"skill",
+    url:"#skill",
   },
   {
-    title: "site",
-    url: "#site",
+    title:"site",
+    url:"#site",
   },
   {
-    title: "port",
-    url: "#port",
+    title:"port",
+    url:"#port",
   },
   {
-    title: "contact",
-    url: "#contact",
+    title:"contact",
+    url:"#contact",
   },
   {
-    title: "footer",
-    url: "#footer",
+    title:"footer",
+    url:"#footer",
   },
 ];
+
 const Header = () => {
   const [show, setShow] = useState(false);
   const toggleMenu = () => {
     setShow((prevShow) => !prevShow);
   };
-
   return (
     <header id="header" role="banner">
-        <div className="header__inner">
-            <div className="header__logo">
-                <h1>
-                    <a href="/">portfolio<em>react</em></a>
-                </h1>
-            </div>
-            <nav className={`header__nav ${show ? "show" :""}`} role="navigation" aria-label="메인메뉴">
-                <ul>
-                  {headerNav.map((nav,key) => (
-                    <li key={key}>
-                      <a href={nav.url}>{nav.title}</a>
-                    </li>
-                  ))}
-                </ul>
-            </nav>
-            <div 
-              className="header__nav__mobile" 
-              id="headerToggle" 
-              aria-controls="primary-menu" 
-              aria-expanded={show ? "true":"false"} 
-              role="button" 
-              tabindex="0" 
-              onClick={toggleMenu}
-              >
-                <span></span>
-            </div>
+      <div className="header__inner">
+        <div className="header__logo">
+          <h1>
+            <a href="/">portfolio<em>react</em></a>
+          </h1>
         </div>
-    </header>  
+        <nav className={`header__nav ${show ? "show" : ""}`} role="navigation" aria-label="메인 메뉴">
+          <ul>
+            {headerNav.map((nav,key) => (
+              <li key={key}>
+                <a href={nav.url}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div 
+          className="header__nav__mobile"
+          id="headerToggle"
+          aria-controls="primary-menu"
+          aria-expanded={show ? "true":"false"}
+          role="button"
+          tabIndex="0"
+          onClick={toggleMenu}
+        >
+          <span></span>
+        </div>
+      </div>
+    </header>
   )
 }
 
