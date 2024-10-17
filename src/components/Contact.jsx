fbd7297f5ee32936f86b6304f52381e0243864a9
Kim YS <withseven87@gmail.com>
2024-10-17 17:17:36 +0900

    연락처완성

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,49 @@
 import React from "react";
 
+const contactText = [
+  {
+    link:"/",
+    title:"KAKAO : Webstroyboy",
+  },
+  {
+    link:"/",
+    title:"mail : WEBSTORYBOY@NAVER.COM",
+  },
+];
 const Contact = () => {
   return (
-    <div>
-      Contact
-    </div>
+    <section id="contact">
+      <div className="contact__inner">
+        <h2 className="contact__title">contact</h2>
+        <div className="contact__lines top" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+        <div className="contact__text">
+          <div className="text">
+            {contactText.map((contact,key) => (
+              <div key={key}>
+                <a href={contact.link} target="_blank">{contact.title}</a>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="contact__lines" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
   )
 }
 
